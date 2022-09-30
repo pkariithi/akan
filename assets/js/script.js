9020487bb3a7040b488c946e44707327f936b978
pkariithi@gmail.com <patrickariithi@gmail.com>
2022-09-30 03:21:23 +0300

    Added css loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  console.log("jQuery working");
+
+  // names
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  // random name - choose a random number between 0 and 6
+  const randomDay = randomNumber(0,6);
+  const randomGender = randomNumber(0,2);
+
+  /*
+   * functions
+   */
+
+  // this function retuns a random number between min (inclusive) and max (inclusive)
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
 });
